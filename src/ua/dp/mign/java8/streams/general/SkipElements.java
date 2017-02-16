131eba3069cbbbc7f727cf97b1dff1ec6d507950
Faresis <nikignatiev@gmail.com>
2017-02-16 14:58:45 +0200

    Stream skip example.

---
+++ src/ua/dp/mign/java8/streams/general/SkipElements.java
@@ -0,0 +1,15 @@
+package ua.dp.mign.java8.streams.general;
+
+import ua.dp.mign.java8.domain.Menu;
+
+import static java.util.stream.Collectors.toList;
+
+public final class SkipElements {
+    public static void main(String[] args) {
+        System.out.println(Menu.getMenu().stream()
+                .filter(d -> d.getCalories() > 300)
+                .skip(2)
+                .collect(toList())
+        );
+    }
+}
