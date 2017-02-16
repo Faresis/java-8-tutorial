0903c14b802ef3e9f22f2510c4b83476a4f69807
Faresis <nikignatiev@gmail.com>
2017-02-16 16:04:58 +0200

    Stream all match method example extended.

---
+++ src/ua/dp/mign/java8/streams/general/AllMatchElements.java
@@ -0,0 +1,11 @@
+package ua.dp.mign.java8.streams.general;
+
+import ua.dp.mign.java8.domain.Menu;
+
+public final class AllMatchElements {
+    public static void main(String[] args) {
+        System.out.println("All dishes are below 1000 calories :" +
+                Menu.getMenu().stream().allMatch(d -> d.getCalories() < 1000)
+        );
+    }
+}
