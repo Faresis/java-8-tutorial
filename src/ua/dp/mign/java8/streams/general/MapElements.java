cad914eeb6e91b30837d42c9d5cec4575c8458d3
Faresis <nikignatiev@gmail.com>
2017-02-16 15:01:32 +0200

    Stream map method  example.

---
+++ src/ua/dp/mign/java8/streams/general/MapElements.java
@@ -0,0 +1,15 @@
+package ua.dp.mign.java8.streams.general;
+
+import ua.dp.mign.java8.domain.Dish;
+import ua.dp.mign.java8.domain.Menu;
+
+import static java.util.stream.Collectors.toList;
+
+public final class MapElements {
+    public static void main(String[] args) {
+        System.out.println(Menu.getMenu().stream()
+                .map(Dish::getName)
+                .collect(toList())
+        );
+    }
+}
