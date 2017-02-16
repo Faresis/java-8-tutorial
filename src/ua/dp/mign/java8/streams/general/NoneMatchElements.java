924a3a1b535e9a5ab6e29b51c85a2eb7b6d4f3f1
Faresis <nikignatiev@gmail.com>
2017-02-16 16:07:25 +0200

    Stream none match method example extended.

---
+++ src/ua/dp/mign/java8/streams/general/NoneMatchElements.java
@@ -0,0 +1,11 @@
+package ua.dp.mign.java8.streams.general;
+
+import ua.dp.mign.java8.domain.Menu;
+
+public final class NoneMatchElements {
+    public static void main(String[] args) {
+        System.out.println("None dishes are above 1000 calories: " +
+                Menu.getMenu().stream().noneMatch(d -> d.getCalories() >= 1000)
+        );
+    }
+}
