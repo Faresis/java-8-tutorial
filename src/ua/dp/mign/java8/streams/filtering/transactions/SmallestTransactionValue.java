dcf19da5bb4bd60f86d59da1cdb85fc585640520
Faresis <nikignatiev@gmail.com>
2017-02-17 17:39:32 +0200

    Smallest transaction value.

--- src/ua/dp/mign/java8/streams/filtering/transactions/SmallestTransactionValue.java
+++ src/ua/dp/mign/java8/streams/filtering/transactions/SmallestTransactionValue.java
@@ -3,12 +3,12 @@ package ua.dp.mign.java8.streams.filtering.transactions;
 import ua.dp.mign.java8.domain.traders.Transaction;
 import ua.dp.mign.java8.domain.traders.Transactions;
 
-public final class HighestTransactionValue {
+public final class SmallestTransactionValue {
     public static void main(String[] args) {
-        System.out.println("Highest transaction value: " +
+        System.out.println("Smallest transaction value: " +
                 Transactions.getTransactions().stream()
                 .mapToInt(Transaction::getValue)
-                .max()
+                .min()
         );
     }
 }
