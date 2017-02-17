9f5178aa42fcbc0d42027e2ff342c5788d81fae0
Faresis <nikignatiev@gmail.com>
2017-02-17 16:11:06 +0200

    Reducing. Number of dishes with map reduce approach.

---
+++ src/ua/dp/mign/java8/streams/reducing/MapReduceCount.java
@@ -0,0 +1,12 @@
+package ua.dp.mign.java8.streams.reducing;
+
+import ua.dp.mign.java8.domain.Menu;
+
+public final class MapReduceCount {
+    public static void main(String[] args) {
+        System.out.println("Number of dishes: " +
+                Menu.getMenu().stream()
+                .map(d -> 1)
+                .reduce(0, (a, b) -> a + b));
+    }
+}
