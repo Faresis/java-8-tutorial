14d38207532b9b24c92d0e41b48a85efcd92adf3
Faresis <nikignatiev@gmail.com>
2017-02-21 13:33:04 +0200

    All traders names sorted alphabetically.
    Joining implementation refactored.

--- src/ua/dp/mign/java8/streams/reducing/AllTradersNames.java
+++ src/ua/dp/mign/java8/streams/reducing/AllTradersNames.java
@@ -3,11 +3,13 @@ package ua.dp.mign.java8.streams.reducing;
 import ua.dp.mign.java8.domain.traders.Trader;
 import ua.dp.mign.java8.domain.traders.Traders;
 
+import static java.util.stream.Collectors.joining;
+
 public final class AllTradersNames {
     public static void main(String[] args) {
         System.out.println(Traders.getTraders().stream()
                 .map(Trader::getName)
                 .sorted()
-                .reduce("", (n1, n2) -> "".equals(n1) ? n2 : n1 + ", " + n2));
+                .collect(joining(", ")));
     }
 }
