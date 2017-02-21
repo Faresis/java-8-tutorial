4768cdaf23fc60984a81511b35291afab8603e36
Faresis <nikignatiev@gmail.com>
2017-02-21 18:06:19 +0200

    Transactions domain model extended.

--- src/ua/dp/mign/java8/domain/traders/Transaction.java
+++ src/ua/dp/mign/java8/domain/traders/Transaction.java
@@ -6,11 +6,13 @@ public final class Transaction {
     private final Trader trader;
     private final int year;
     private final int value;
+    private final Currency currency;
 
-    public Transaction(Trader trader, int year, int value) {
+    public Transaction(Trader trader, int year, int value, Currency currency) {
         this.trader = trader;
         this.year = year;
         this.value = value;
+        this.currency = currency;
     }
 
     public Trader getTrader() {
@@ -25,12 +27,23 @@ public final class Transaction {
         return value;
     }
 
+    public Currency getCurrency() {
+        return currency;
+    }
+
     @Override
     public String toString() {
         return new StringJoiner(", ", Transaction.class.getSimpleName() + "[", "]")
                 .add("trader=" + trader)
                 .add("year=" + year)
                 .add("value=" + value)
+                .add("currency=" + currency)
                 .toString();
+    }
+
+    public enum Currency {
+        USD,
+        EURO,
+        UAH
     }
 }
--- src/ua/dp/mign/java8/domain/traders/Transactions.java
+++ src/ua/dp/mign/java8/domain/traders/Transactions.java
@@ -3,14 +3,18 @@ package ua.dp.mign.java8.domain.traders;
 import java.util.Arrays;
 import java.util.List;
 
+import static ua.dp.mign.java8.domain.traders.Transaction.Currency.EURO;
+import static ua.dp.mign.java8.domain.traders.Transaction.Currency.UAH;
+import static ua.dp.mign.java8.domain.traders.Transaction.Currency.USD;
+
 public final class Transactions {
     private static final List<Transaction> TRANSACTIONS = Arrays.asList(
-            new Transaction(Traders.BRIAN, 2011, 300),
-            new Transaction(Traders.RAOUL, 2012, 1000),
-            new Transaction(Traders.RAOUL, 2011, 400),
-            new Transaction(Traders.MARIO, 2012, 710),
-            new Transaction(Traders.MARIO, 2012, 700),
-            new Transaction(Traders.ALAN, 2012, 950)
+            new Transaction(Traders.BRIAN, 2011, 300, USD),
+            new Transaction(Traders.RAOUL, 2012, 1000, USD),
+            new Transaction(Traders.RAOUL, 2011, 400, EURO),
+            new Transaction(Traders.MARIO, 2012, 710, EURO),
+            new Transaction(Traders.MARIO, 2012, 700, UAH),
+            new Transaction(Traders.ALAN, 2012, 950, USD)
     );
 
     public static List<Transaction> getTransactions() {
