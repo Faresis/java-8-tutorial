f4872ddafa1ea585f6c1f4573de969d58a408510
Faresis <nikignatiev@gmail.com>
2017-02-23 17:11:10 +0200

    Dishes count with counting collector. Stream.count() example added.

--- src/ua/dp/mign/java8/streams/collection/DishesCount.java
+++ src/ua/dp/mign/java8/streams/collection/DishesCount.java
@@ -7,5 +7,7 @@ import static java.util.stream.Collectors.counting;
 public final class DishesCount {
     public static void main(String[] args) {
         System.out.println(Menu.getMenu().stream().collect(counting()));
+
+        System.out.println(Menu.getMenu().stream().count());
     }
 }
