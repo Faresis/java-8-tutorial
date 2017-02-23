87c66d6a9249f3240a44ef590e77f40ab16e31f6
Faresis <nikignatiev@gmail.com>
2017-02-23 17:10:27 +0200

    Dishes count with counting collector.

---
+++ src/ua/dp/mign/java8/streams/collection/DishesCount.java
@@ -0,0 +1,11 @@
+package ua.dp.mign.java8.streams.collection;
+
+import ua.dp.mign.java8.domain.dishes.Menu;
+
+import static java.util.stream.Collectors.counting;
+
+public final class DishesCount {
+    public static void main(String[] args) {
+        System.out.println(Menu.getMenu().stream().collect(counting()));
+    }
+}
