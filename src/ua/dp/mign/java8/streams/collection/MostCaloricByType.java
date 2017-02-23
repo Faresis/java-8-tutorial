1be9ab769d3c1056cb7982124d9de5873dd576e0
Faresis <nikignatiev@gmail.com>
2017-02-23 17:59:38 +0200

    Collector results post processing with collectingAndThen.

--- src/ua/dp/mign/java8/streams/collection/MostCaloricByType.java
+++ src/ua/dp/mign/java8/streams/collection/MostCaloricByType.java
@@ -3,7 +3,10 @@ package ua.dp.mign.java8.streams.collection;
 import ua.dp.mign.java8.domain.dishes.Dish;
 import ua.dp.mign.java8.domain.dishes.Menu;
 
+import java.util.Optional;
+
 import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.maxBy;
 
@@ -11,6 +14,10 @@ public final class MostCaloricByType {
     public static void main(String[] args) {
         System.out.println(Menu.getMenu().stream()
                 .collect(groupingBy(Dish::getType, maxBy(comparing(Dish::getCalories))))
+        );
+
+        System.out.println(Menu.getMenu().stream()
+                .collect(groupingBy(Dish::getType, collectingAndThen(maxBy(comparing(Dish::getCalories)), Optional::get)))
         );
     }
 }
