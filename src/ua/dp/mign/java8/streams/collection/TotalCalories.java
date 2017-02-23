f3a9967ec20a359a362b4281dc1df4b9adddba6d
Faresis <nikignatiev@gmail.com>
2017-02-23 17:47:13 +0200

    Sum of all dish calories using reducing collector.

--- src/ua/dp/mign/java8/streams/collection/TotalCalories.java
+++ src/ua/dp/mign/java8/streams/collection/TotalCalories.java
@@ -1,12 +1,16 @@
 package ua.dp.mign.java8.streams.collection;
 
 import ua.dp.mign.java8.domain.dishes.Dish;
-import ua.dp.mign.java8.domain.dishes.Menu;
 
+import static java.lang.System.out;
+import static java.util.stream.Collectors.reducing;
 import static java.util.stream.Collectors.summingInt;
+import static ua.dp.mign.java8.domain.dishes.Menu.getMenu;
 
 public final class TotalCalories {
     public static void main(String[] args) {
-        System.out.println(Menu.getMenu().stream().collect(summingInt(Dish::getCalories)));
+        out.println(getMenu().stream().collect(summingInt(Dish::getCalories)));
+
+        out.println(getMenu().stream().collect(reducing(0, Dish::getCalories, (i, z) -> i + z)));
     }
 }
