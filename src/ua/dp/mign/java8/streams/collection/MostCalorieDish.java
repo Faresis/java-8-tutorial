657ec051a098f484458e74e7de2864694cbcdfd4
Faresis <nikignatiev@gmail.com>
2017-02-23 17:49:16 +0200

    Most calorie dish with reducing collector example.

--- src/ua/dp/mign/java8/streams/collection/MostCalorieDish.java
+++ src/ua/dp/mign/java8/streams/collection/MostCalorieDish.java
@@ -1,15 +1,22 @@
 package ua.dp.mign.java8.streams.collection;
 
 import ua.dp.mign.java8.domain.dishes.Dish;
-import ua.dp.mign.java8.domain.dishes.Menu;
 
+import static java.lang.System.out;
 import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.reducing;
+import static ua.dp.mign.java8.domain.dishes.Menu.getMenu;
 
 public final class MostCalorieDish {
     public static void main(String[] args) {
-        System.out.println(Menu.getMenu().stream()
+        out.println(getMenu().stream()
                 .collect(maxBy(comparingInt(Dish::getCalories)))
+        );
+
+        out.println(getMenu().stream()
+                .collect(reducing((d1, d2) ->
+                        d1.getCalories() > d2.getCalories() ? d1 : d2))
         );
     }
 }
