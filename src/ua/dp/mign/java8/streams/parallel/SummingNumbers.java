22e91a3d1e2f9cb26db60444d525f5b9981fa4df
Faresis <nikignatiev@gmail.com>
2017-03-02 10:23:42 +0200

    Stream parallel processing example with side effect function.

--- src/ua/dp/mign/java8/streams/parallel/SummingNumbers.java
+++ src/ua/dp/mign/java8/streams/parallel/SummingNumbers.java
@@ -40,11 +40,32 @@ public final class SummingNumbers {
                 .reduce(0L, Long::sum);
     }
 
+    public static long sideEffectSum(long n) {
+        Accumulator accumulator = new Accumulator();
+        LongStream.rangeClosed(1, n).forEach(accumulator::add);
+        return accumulator.total;
+    }
+
+    public static long sideEffectParallelSum(long n) {
+        Accumulator accumulator = new Accumulator();
+        LongStream.rangeClosed(1, n).parallel().forEach(accumulator::add);
+        return accumulator.total;
+    }
+
+    private static class Accumulator {
+        long total = 0;
+
+        void add(long value) {
+            total += value;
+        }
+    }
+
     public static long measureSumPerf(Function<Long, Long> adder, long n) {
         long fastest = Long.MAX_VALUE;
         for (int i = 0; i < 10; i++) {
             long start = System.nanoTime();
             long sum = adder.apply(n);
+            System.out.println("Result: " + sum);
             long duration = (System.nanoTime() - start) / 1_000_000;
             if (duration < fastest) fastest = duration;
         }
@@ -52,10 +73,12 @@ public final class SummingNumbers {
     }
 
     public static void main(String[] args) {
-        out.println("Sequental sum done in: " + measureSumPerf(SummingNumbers::sequentalSum, 10_000_000) + "msecs");
+/*        out.println("Sequental sum done in: " + measureSumPerf(SummingNumbers::sequentalSum, 10_000_000) + "msecs");
         out.println("Iterative sum done in: " + measureSumPerf(SummingNumbers::iterativeSum, 10_000_000) + "msecs");
         out.println("Parallel sum done in: " + measureSumPerf(SummingNumbers::parallelSum, 10_000_000) + "msecs");
         out.println("Ranged sum done in: " + measureSumPerf(SummingNumbers::rangedSum, 10_000_000) + "msecs");
-        out.println("Ranged parallel sum done in: " + measureSumPerf(SummingNumbers::rangedParallelSum, 10_000_000) + "msecs");
+        out.println("Ranged parallel sum done in: " + measureSumPerf(SummingNumbers::rangedParallelSum, 10_000_000) + "msecs");*/
+
+        out.println("Side effect parallel sum done in: " + measureSumPerf(SummingNumbers::sideEffectParallelSum, 10_000_000) + "msecs");
     }
 }
