2ac2869811888be89f508b3b6b15f3e391ba56b3
Faresis <nikignatiev@gmail.com>
2017-04-30 13:58:54 +0300

    Fields extraction using map method example.

---
+++ src/ua/dp/mign/java8/streams/collection/MapDemo.java
@@ -0,0 +1,25 @@
+package ua.dp.mign.java8.streams.collection;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class MapDemo {
+    public static void main(String[] args) {
+        Stream<Employee> employees = Stream.of(new Employee("Alice"), new Employee("Bob"), new Employee("Smith"));
+        List<String> names = employees.map(Employee::getName).collect(Collectors.toList());
+        System.out.println(names);
+    }
+
+    private static class Employee {
+        private final String name;
+
+        public Employee(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
