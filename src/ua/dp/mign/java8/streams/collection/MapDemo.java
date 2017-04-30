f463ca9709baa58c58cf61963be74e37bae61f58
Faresis <nikignatiev@gmail.com>
2017-04-30 14:01:23 +0300

    Primitive specialized stream example.

--- src/ua/dp/mign/java8/streams/collection/MapDemo.java
+++ src/ua/dp/mign/java8/streams/collection/MapDemo.java
@@ -2,6 +2,7 @@ package ua.dp.mign.java8.streams.collection;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public final class MapDemo {
@@ -9,6 +10,9 @@ public final class MapDemo {
         Stream<Employee> employees = Stream.of(new Employee("Alice"), new Employee("Bob"), new Employee("Smith"));
         List<String> names = employees.map(Employee::getName).collect(Collectors.toList());
         System.out.println(names);
+
+        IntStream intStream = names.stream().mapToInt(String::length);
+        System.out.println("Max " + intStream.max().getAsInt());
     }
 
     private static class Employee {
